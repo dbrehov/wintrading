9e9e01ff7c932f8da26be4dad4a4b82f1633f72f
mac <mac@MacBook-Pro-mac.local>
2026-09-19 15:01:16 -0400

    fix: update to verified working telegram bot token

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
 export default {
     chatId: '5114383390',
-    OUR_BOT_TOKEN: '7175657723:AAH7Goj4JZtRt6ieOXx7C7FSTRPWwLq-Whc'
+    OUR_BOT_TOKEN: '5381437807:AAGydymIPxw9a5qlqyYOtog3qySK7fGnCqc'
 };
